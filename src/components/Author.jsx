e6c62f219125ba9379b04e33973838584fdd8268
Phille91 <philipdale91@gmail.com>
2026-09-08 13:09:10 +0200

    refactor Author component to display author image and bio

--- src/components/Author.jsx
+++ src/components/Author.jsx
@@ -1,19 +1,18 @@
-import { storyblokEditable, StoryblokServerComponent, StoryblokServerRichText } from "@storyblok/react/rsc";
+import { storyblokEditable } from "@storyblok/react/rsc";
 import Link from "next/link";
 
 export default function Author({ blok }) {
-	const authors = (blok.author ?? []).filter((a) => a?.content);
-
 	return (
-		<article {...storyblokEditable(blok)}>
-			<h1>{blok.title}</h1>
-
-			{authors.length > 0 && (
-				<p>Written by: {authors.map((a) => a.content.name).join(', ')}</p>
+		<article className="author" {...storyblokEditable(blok)}>
+			{blok.image?.filename && (
+				<img src={blok.image.filename} alt={blok.name} />
 			)}
 
-			<StoryblokServerRichText doc={blok.body} />
-			<Link href="/articles">Back to articles</Link>
+			<h1>{blok.name}</h1>
+
+			{blok.bio && <p>{blok.bio}</p>}
+
+			<Link href="/authors">Back to authors</Link>
 		</article>
 	);
 }
