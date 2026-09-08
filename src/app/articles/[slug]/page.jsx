1ee85ae97e06814f4a88914b56d171d2e6dc6e45
Oscar <o.frykedal@gmail.com>
2026-09-08 14:12:25 +0200

    added generate static params to /articles/[slug] and /authors/[slug]

--- src/app/articles/[slug]/page.jsx
+++ src/app/articles/[slug]/page.jsx
@@ -1,38 +1,52 @@
-import { getStoryblokApi } from "@/lib/storyblok";
-import { StoryblokServerComponent } from "@storyblok/react/rsc";
-import { notFound } from "next/navigation";
+import { getStoryblokApi } from '@/lib/storyblok';
+import { StoryblokServerComponent } from '@storyblok/react/rsc';
+import { notFound } from 'next/navigation';
+
+export async function generateStaticParams() {
+	const storyblokApi = getStoryblokApi();
+
+	const { data } = await storyblokApi.getStories({
+		version: 'published',
+		starts_with: 'articles/',
+		content_type: 'article',
+	});
+
+	return data.stories.map((story) => ({
+		slug: story.slug,
+	}));
+}
 
 export async function generateMetadata({ params }) {
-  const { slug } = await params;
+	const { slug } = await params;
 
-  const storyblokApi = getStoryblokApi();
+	const storyblokApi = getStoryblokApi();
 
-  const { data } = await storyblokApi.get(`cdn/stories/articles/${slug}`, {
-    version: "draft",
-  });
+	const { data } = await storyblokApi.get(`cdn/stories/articles/${slug}`, {
+		version: 'draft',
+	});
 
-  // console.log(JSON.stringify(data.story, null, 2));
+	// console.log(JSON.stringify(data.story, null, 2));
 
-  return {
-    title: data.story.name,
-    description: data.story.content.summary,
-  };
+	return {
+		title: data.story.name,
+		description: data.story.content.summary,
+	};
 }
 
 export default async function ArticlePage({ params }) {
-  const { slug } = await params;
-  const storyblokApi = getStoryblokApi();
+	const { slug } = await params;
+	const storyblokApi = getStoryblokApi();
 
-  let story;
-  try {
-    const { data } = await storyblokApi.get(`cdn/stories/articles/${slug}`, {
-      version: "draft",
-      resolve_relations: ['article.author'],
-    });
-    story = data.story;
-  } catch {
-    notFound();
-  }
+	let story;
+	try {
+		const { data } = await storyblokApi.get(`cdn/stories/articles/${slug}`, {
+			version: 'draft',
+			resolve_relations: ['article.author'],
+		});
+		story = data.story;
+	} catch {
+		notFound();
+	}
 
-  return <StoryblokServerComponent blok={story.content} />;
+	return <StoryblokServerComponent blok={story.content} />;
 }
--- src/app/authors/[slug]/page.jsx
+++ src/app/authors/[slug]/page.jsx
@@ -1,25 +1,39 @@
-import { getStoryblokApi } from "@/lib/storyblok";
-import { StoryblokServerComponent } from "@storyblok/react/rsc";
-import { notFound } from "next/navigation";
+import { getStoryblokApi } from '@/lib/storyblok';
+import { StoryblokServerComponent } from '@storyblok/react/rsc';
+import { notFound } from 'next/navigation';
+
+export async function generateStaticParams() {
+	const storyblokApi = getStoryblokApi();
+
+	const { data } = await storyblokApi.getStories({
+		version: 'published',
+		starts_with: 'authors/',
+		content_type: 'author',
+	});
+
+	return data.stories.map((story) => ({
+		slug: story.slug,
+	}));
+}
 
 export default async function AuthorPage({ params }) {
-  const { slug } = await params;
-  const storyblokApi = getStoryblokApi();
+	const { slug } = await params;
+	const storyblokApi = getStoryblokApi();
 
-  let authorStory;
-  try {
-    const { data } = await storyblokApi.get(`cdn/stories/authors/${slug}`, {
-      version: "draft",
-    });
-    authorStory = data.story;
-  } catch {
-    notFound();
-  }
+	let authorStory;
+	try {
+		const { data } = await storyblokApi.get(`cdn/stories/authors/${slug}`, {
+			version: 'draft',
+		});
+		authorStory = data.story;
+	} catch {
+		notFound();
+	}
 
-  return (
-    <StoryblokServerComponent
-      blok={authorStory.content}
-      uuid={authorStory.uuid}
-    />
-  );
+	return (
+		<StoryblokServerComponent
+			blok={authorStory.content}
+			uuid={authorStory.uuid}
+		/>
+	);
 }
