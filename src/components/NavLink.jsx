07d42d6881126396b101fc83253457062d08b32a
Oscar <o.frykedal@gmail.com>
2026-09-09 11:09:54 +0200

    added multi-level menu styling for NavLink component

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,27 +1,3 @@
-// import Link from 'next/link';
-// import {
-// 	storyblokEditable
-// } from '@storyblok/react/rsc';
-// import { resolveLink } from '@/lib/resolveLink';
-
-// export default function NavLink({ blok }) {
-// 	if (!blok) return null;
-
-// 	const href = resolveLink(blok.link);
-// 	const isExternal = blok.link?.url?.startsWith('http');
-// 	return (
-// 		<li {...storyblokEditable(blok)}>
-// 			{isExternal ? (
-// 				<a href={href} target="_blank" rel="noopener noreferrer">
-// 					{blok.label}
-// 				</a>
-// 			) : (
-// 				<Link href={href} className="transition-colors hover:text-neutral-900">{blok.label}</Link>
-// 			)}
-// 		</li>
-// 	);
-// }
-
 import Link from 'next/link';
 import { storyblokEditable } from '@storyblok/react/rsc';
 import { resolveLink } from '@/lib/resolveLink';
