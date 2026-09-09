79797872b18cfd79dbb8045855080b44aa1159a2
Phille91 <philipdale91@gmail.com>
2026-09-09 10:14:04 +0200

    refactor sitemap function to exclude home page from generated URLs

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -11,17 +11,19 @@ export default async function sitemap() {
   });
 
   const pages = data.stories
-    .filter((story) => !story.full_slug.startsWith("config"))
+    .filter((story) => !story.full_slug.startsWith("config") && story.full_slug !== "home")
     .map((story) => ({
       url: `${siteUrl}/${story.full_slug}`,
       lastModified: story.published_at ?? new Date(),
     }));
 
-  return [
-    {
-      url: siteUrl,
-      lastModified: new Date(),
-    },
-    ...pages,
-  ];
+const homeStory = data.stories.find((story) => story.full_slug === "home");
+
+return [
+  {
+    url: siteUrl,
+    lastModified: homeStory?.published_at ?? new Date(),
+  },
+  ...pages, 
+];
 }
